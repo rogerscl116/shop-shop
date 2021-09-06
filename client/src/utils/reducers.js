56275d71d21e3cee0d43c5bac63e6b4871649be4
charity.rogers <rogerscl116@gmail.com>
2021-09-06 13:28:04 -0500

    create category tests and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -8,9 +9,21 @@ import {
     switch (action.type) {
       // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
       case UPDATE_PRODUCTS:
-        return {
-          ...state,
-          products: [...action.products],
+          return {
+            ...state,
+            products: [...action.products],
+        };
+      // if action type value is the balue of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+          return {
+            ...state,
+            categories: [...action.categories]
+        };
+
+      case UPDATE_CURRENT_CATEGORY:
+          return {
+            ...state,
+            currentCategory: action.currentCategory
         };
   
       // if it's none of these actions, do not update state at all and keep things the same!
@@ -18,3 +31,7 @@ import {
         return state;
     }
   };
+
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
