da68f38006c56f9dc281d9b120a51183940474cf
charity.rogers <rogerscl116@gmail.com>
2021-09-07 22:13:51 -0500

    implement idb storage for single products

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -12,6 +12,7 @@ import {
   UPDATE_PRODUCTS,
 } from '../utils/actions';
 import Cart from '../components/Cart';
+import { idbPromise } from "../utils/helpers";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -48,15 +49,31 @@ function Detail() {
   };
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
